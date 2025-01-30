180121c4404ef4f85e027b64299e79249b5e3862
first <murdasovigor17@gmail.com>
2025-01-30 20:32:40 +0300

    nothing

--- trash/1.py
+++
@@ -1,4 +0,0 @@
-star_3 = reviews.loc[reviews.points >= 95 | reviews.country == "Canada"]
-star_2 = reviews.loc[85 <= reviews.points < 95]
-star_1 = reviews.loc[85 > rewiews.points]
-star_ratings = pd.Series({"1"})
