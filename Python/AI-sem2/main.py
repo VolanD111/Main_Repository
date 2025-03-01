956583cfdd299db747c6f0df692bb739a4abd2a3
Igor Murdasov <murdasovigor17@gmail.com>
2025-03-01 11:49:58 +0300

    fix_md_wb

--- Python/AI-sem2/main.py
+++ Python/AI-sem2/main.py
@@ -1,36 +1,10 @@
-class Tree:
-    def __init__(self, data):
-        self.left = None
-        self.right = None
-        self.data = data
-        self.check = True
-    def PrintTree(self, level=0, prefix="Root: "):
-        #  Вывод дерева с отступами
-        print(" " * (level * 4) + prefix + str(self.data))
-        if self.left:
-            self.left.PrintTree(level + 1, "L--- ")
-        if self.right:
-            self.right.PrintTree(level + 1, "R--- ")
-    def add_value(self, newValue):
-        if self.data > newValue:
-            if self.left is None:
-                self.left = Tree(newValue)
-            elif self.right is None:
-                self.right = Tree(newValue)
-            else:
-                if self.check:
-                    self.left.add_value(newValue)
-                    self.check = False
-                else:
-                    self.right.add_value(newValue)
-                    self.check = True
+import pandas as pd
 
-test = Tree(25)
-test.add_value(20)
-test.add_value(23)
-test.add_value(18)
-test.add_value(16)
-test.add_value(13)
-test.add_value(12)
-#test.add_value(11)
-print(test.PrintTree())
+# Пример данных
+data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+df = pd.DataFrame(data)
+print(df)
+
+# Используем iloc для доступа по позициям
+print(df.iloc[1])  # Доступ по позиции строки
+print(df.iloc[1, 1])  # Доступ по позиции строки и позиции столбца
